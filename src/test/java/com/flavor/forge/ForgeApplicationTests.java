9f7cf5e5b3df35c1f0f2a67b265313f928537965
Eric w <none>
2024-11-01 15:57:34 -0700

    Added a Service check tests

--- src/test/java/com/flavor/forge/ForgeApplicationTests.java
+++ src/test/java/com/flavor/forge/ForgeApplicationTests.java
@@ -1,13 +1,42 @@
 package com.flavor.forge;
 
+import com.flavor.forge.Service.CommentService;
+import com.flavor.forge.Service.RecipeService;
+import com.flavor.forge.Service.SearchService;
+import com.flavor.forge.Service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ForgeApplicationTests {
 
+	@Autowired
+	private CommentService commentService;
+	@Autowired
+	private RecipeService recipeService;
+	@Autowired
+	private SearchService searchService;
+	@Autowired
+	private UserService userService;
+
 	@Test
-	void contextLoads() {
+	void commentServiceLoads() throws Exception {
+		assertThat(commentService).isNotNull();
+	}
+	@Test
+	void RecipeServiceLoads() throws Exception {
+		assertThat(recipeService).isNotNull();
+	}
+	@Test
+	void searchServiceLoads() throws Exception {
+		assertThat(searchService).isNotNull();
+	}
+	@Test
+	void userSericeLoads() throws Exception {
+		assertThat(userService).isNotNull();
 	}
 
 }
